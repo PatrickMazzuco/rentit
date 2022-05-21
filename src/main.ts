56610a188c6494ce9e025ba0fef883ee8307777e
PatrickMazzuco <patrickmazzuco@gmail.com>
2022-05-21 17:10:18 -0300

    chore: setup debugging

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ const server = express();
 
 server.use(express.json());
 
-server.get("", (req, res) =>
+server.get("", (req, res) => {
   res.json({
-    message: "Hello World",
+    message: "Hello World!",
     status: 200,
-  }),
-);
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
